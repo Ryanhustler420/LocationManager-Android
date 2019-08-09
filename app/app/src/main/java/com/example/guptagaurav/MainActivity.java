24492231a9fc675bd879821d64939c3f019a77fb
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-09 20:13:40 +0530

    Put Some Member Variables and  Initialize Them

--- app/app/src/main/java/com/example/guptagaurav/MainActivity.java
+++ app/app/src/main/java/com/example/guptagaurav/MainActivity.java
@@ -1,13 +1,105 @@
 package com.example.guptagaurav;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.location_result)
+    TextView txtLocationResult;
+
+    @BindView(R.id.updated_on)
+    TextView txtUpdatedOn;
+
+    @BindView(R.id.btn_start_location_updates)
+    Button btnStartUpdates;
+
+    @BindView(R.id.btn_stop_location_updates)
+    Button btnStopUpdates;
+
+    // location last update time
+    private String mLastUpdateTime;
+
+    // location updates interval - 10sec
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    // fastest updates interval - 5sec
+    // location updates will be received if another app is requesting the locations
+    // than our app can handle
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 5000;
+
+    private static final int REQUEST_CHECK_SETTINGS = 100;
+
+    // few location related apis which we will use here for better search
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SettingsClient mSettingsClient;
+    private LocationRequest mLocationRequest;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationCallback mLocationCallback;
+    private Location mCurrentLocation;
+
+
+    // boolean flag to toggle the ui
+    private Boolean mRequestingLocationUpdates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // binding current view with this class
+        ButterKnife.bind(this);
+        
+        // initialize the necessary libraries
+        init();
+
+        // restore the values from saved instance state
+        restoreValuesFromBundle(savedInstanceState);
+        
+    }
+
+    private void init() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                //  location is received
+                mCurrentLocation = locationResult.getLastLocation();
+                mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+
+                // we'll update the UI here
+            }
+
+            @Override
+            public void onLocationAvailability(LocationAvailability locationAvailability) {
+                super.onLocationAvailability(locationAvailability);
+            }
+        };
+    }
+
+    private void restoreValuesFromBundle(Bundle savedInstanceState) {
     }
 }
